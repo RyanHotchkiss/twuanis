98136043a301449e4528876e850fe18ed19eb425
Cassidy and Dad Hotchkiss <cassidyanddadhotchkiss@Cassidys-iMac.local>
2026-06-08 16:20:29 -0600

    Pass token to Twilio template

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -18,10 +18,21 @@ console.log('TWILIO ACCOUNT SID:', process.env.TWILIO_ACCOUNT_SID)
 console.log('TWILIO WHATSAPP NUMBER:', process.env.TWILIO_WHATSAPP_NUMBER)
 console.log('TWILIO TO:', `whatsapp:+506${to}`)
 
+console.log(
+  'CONTENT VARIABLES:',
+  JSON.stringify({
+    1: body
+  })
+)
+
     const message = await client.messages.create({
       from: process.env.TWILIO_WHATSAPP_NUMBER!,
       to: `whatsapp:+506${to}`,
       contentSid: 'HX7b16956a26f1d43a6ef77784cba5ab98',
+
+      contentVariables: JSON.stringify({
+        1: body
+      })
     })
 
     console.log('WhatsApp sent:', message.sid)
